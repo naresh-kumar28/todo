220dac91975000353d8a4e0341039c395d660d75
Naresh Kumar <nareshkumarsah780@gmail.com>
2026-06-23 11:43:24 +0530

    Update settings to use environment variable for SECRET_KEY and add .gitignore and requirements.txt files

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-blpimfjf_5y5#k6vv1oce3^i)xxx!$&-jbq3q7tb-k_4-inb$m'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
